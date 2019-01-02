f426ef8c0c3a75aa2481f3f8dd31617bc9fe3b62
ArthurBernard <arthur.bernard.92@gmail.com>
2019-01-02 09:04:20 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages
+
+setup(
+    name='Strategy_Manager',
+    version='0.1',
+    description='Manager of financial algorithmic strategies [In progress]',
+    url='https://github.com/ArthurBernard/Strategy_Manager',
+    author=['Arthur Bernard'],
+    author_email='arthur.bernard.92@gmail.com',
+    packages=find_packages(),
+)
