285ac847a90a4d5a0faebe5ffeea7712956579f2
ArthurBernard <arthur.bernard.92@gmail.com>
2019-01-11 17:33:15 +0100

    create script strategy manager

---
+++ strategy_manager/strategy_manager.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pickle import Pickler, Unpickler
+import time
+
+"""
+TODO list:
+    - Create a class strategy manager (or a bot object).
+    - Init parameters: underlying, strategy function, step time, value allow 
+    to this strategy.
+    - Methods: ... ?
+"""
