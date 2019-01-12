495c14577e329f5ab6079996e92bfa1c6051c6d0
ArthurBernard <arthur.bernard.92@gmail.com>
2019-01-12 12:04:37 +0100

    update strategy_manager

--- strategy_manager/strategy_manager.py
+++ strategy_manager/strategy_manager.py
@@ -11,3 +11,48 @@ TODO list:
     to this strategy.
     - Methods: ... ?
 """
+
+class StrategyManager:
+	"""
+    Description..
+    
+    Methods
+    -------
+    todo: - ?
+
+    tofinish: - Init;
+              - Call;
+              - Isovol;
+
+    Attributes
+    ----------
+
+    """
+    def __init__(self, timestep, underlying, strategy):
+        """
+        Parameters
+        ----------
+        :timestep: int
+            Number of seconds between two samples.
+        :underlying: str or list ?
+            Name of the underlying or list of data needed.
+        :strategy: function ? str ? callable ?
+            Strategy function.
+        """
+        self.timestep = timestep
+        self.underlying = underlying
+        self.strategy = strategy
+
+    def __call__(self, *args, **kwargs):
+        """ 
+        Call strategy signal.
+        Apply iso-volatility ? 
+        """
+    	self.signal = self.strategy(*args, **kwargs)
+    	pass
+
+    def _isovol(self):
+    	""" 
+    	Iso-volatility method
+    	"""
+    	pass
