0ad9b847b6789208276e2af00a83627fbaa17685
ArthurBernard <arthur.bernard.92@gmail.com>
2019-01-13 20:57:18 +0100

    update data_loader

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -23,7 +23,16 @@ class DataLoader:
 
     Attributes
     ----------
-
+    :timestep: int
+        Number of seconds between two samples.
+    :underlying: str or list ?
+        Name of the underlying or list of data needed.
+    :since: data (%d-%m-%y %H:%M:%S) or timestamp.
+        Date of first observation of data sample (default is the first 
+        observation of the day). This parameter is ignored if sample_size 
+        is specified.
+    :last: data (%d-%m-%y %H:%M:%S) or timestamp.
+        Date of last observation of data sample (default is now).
     """
     def __init__(self, timestep, underlying, sample_size=None, since=None, last=None):
         """
@@ -44,30 +53,11 @@ class DataLoader:
         """
         self.timestep = timestep
         self.underlying = underlying
-        self.last = self._date_to_TS(last)
+        self.last = date_to_TS(last)
         if sample_size is not None:
             self.since = self.last - timestep * sample_size
         elif since is not None:
-            self.since = self._date_to_TS(since)
+            self.since = date_to_TS(since)
         else:
             sample_size = (last % 86400) * 86400
             self.since = self.last - timestep * sample_size
-        
-    def _date_to_TS(self, date):
-        """
-        Parameters
-        ----------
-        :date: int, str, date, etc ?
-            Date to convert in timestamp
-
-        Return
-        ------
-        Timestamp of the date.
-        """
-        if isinstance(date, int):
-            return date
-        elif isinstance(date, str):
-            pass
-        else:
-            print('Date format not allowed')
-            raise Error
