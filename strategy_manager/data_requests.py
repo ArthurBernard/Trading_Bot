9c0a42493a50dec0b642d6254aa1aeac7c097294
ArthurBernard <arthur.bernard.92@gmail.com>
2019-03-09 11:29:30 +0100

    add request data base function

--- strategy_manager/data_requests.py
+++ strategy_manager/data_requests.py
@@ -5,9 +5,11 @@
 import json 
 import time
 import sys
+from pickle import Unpickler
 
 # Import external packages
 import requests
+import pandas as pd
 
 __all__ = ['DataRequests']
 
@@ -109,6 +111,78 @@ class DataRequests:
         self.time_step = time_step
         self.kwargs = kwargs
         return self
+
+
+def data_base_requests(assets, ohlcv, frequency=60, start=None, end=None):
+    """ Function to request in the data base one or several ohlcv data assets
+    from a specified date to an other specified data and at a specified 
+    frequency. 
+
+    Parameters
+    ----------
+    assets : str or list of str
+        Id(s) of the asset(s) to requests.
+    ohlcv : str or list of str
+        Kind of price data to requests, following are available 'o' to open, 
+        'h' to high, 'l' to low, 'c' to close and 'v' to volume.
+    frequency : int, optional
+        Number of second between two data observations (> 60).
+    start : int, optional
+        Timestamp to start data request, default start request at last data 
+        availabe.
+    end : int, optional
+        Timestamp to end data request, default end request at last data 
+        available.
+
+    Returns
+    -------
+    data : pd.DataFrame
+        A data frame with the data requested.
+
+    Examples
+    --------
+
+    See Also
+    --------
+
+    """
+    if end is None:
+        end = int(time.time()) // 60 * 60
+    data = pd.DataFrame()
+    for asset in assets:
+        if start is None:
+            date = time.strftime('%y-%m-%d', time.gmtime(time.time()))
+            path = '../data_base/' + asset + '/' + date
+            df = _data_base_requests(path, slice(None), ohlcv).iloc[-1, :]
+        else:
+            df = pd.DataFrame()
+            for row_slice in _set_row_slice(start, end, frequency):
+                date = time.strftime('%y-%m-%d', time.gmtime(row_slice[0]))
+                path = '../data_base/' + asset + '/' + date
+                subdf = _data_base_requests(path, row_slice, ohlcv)
+                df.append(subdf)
+        data = data.join(df, rsuffix=asset)
+
+    return data
+
+
+def _data_base_requests(path, row_slice, col_slice):
+    # Load data base
+    with open(path, 'rb') as f:
+        df = Unpickler(f).load()
+    # Return specified data
+    return df.loc[row_slice, col_slice]
+
+
+def _set_row_slice(start, end, frequency):
+    i = 0
+    row_slices = []
+    end += frequency
+    while i < (end - start) // 86400:
+        row_slice += [range(start, min(start + 86400, end), frequency)]
+        start += 86400
+    return row_slice
+
 
 if __name__ == '__main__':
     import doctest
