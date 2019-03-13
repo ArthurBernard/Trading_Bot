979b9e52607e868ce94fa218175331f32a7a2660
ArthurBernard <arthur.bernard.92@gmail.com>
2019-03-13 21:04:53 +0100

    update

--- strategy_manager/main.py
+++ strategy_manager/main.py
@@ -2,16 +2,17 @@
 # coding: utf-8
 
 # Import built-in packages
-import json 
 import time
 import sys
 
 # Import external packages
 
 # Import internal packages
-from . import StrategyManager
+from .manager import StrategyManager, DataManager
+from .tools.utils import load_config_params
 
 __all__ = ['run_bot']
+
 
 def run_bot(strat_id, path='../strategies/'):
     """ Run a bot for specified configuration file.
@@ -27,7 +28,7 @@ def run_bot(strat_id, path='../strategies/'):
     if path[-1] != '/':
         path += '/'
     data_cfg = load_config_params(path + strat_id + '.cfg')
-    
+
     # Get parameters for strategy manager object
     strat_manager_params = data_cfg['strat_manager_instance']
     # Get parameters for strategy function
@@ -35,33 +36,35 @@ def run_bot(strat_id, path='../strategies/'):
     strat_kwargs = data_cfg['strategy_instance']['kwargs_params']
     # Init strategy manager
     strat_manager = StrategyManager(**strat_manager_params)
-    
+
     # Get parameters for data requests
     data_requests_params = data_cfg['get_data_instance']
     data_requests_args = data_requests_params.pop('args_params')
     data_requests_kwargs = data_requests_params.pop('kwargs_params')
     # TODO : Set or verify data requests manager configuration
-    # data_manager = DataManager(**data_requests_params)
-    
+    data_manager = DataManager(**data_requests_params)
+
     # Get parameters for pre order configuration
     pre_order_params = data_cfg['pre_order_instance']
     # Set pre order configuration
     strat_manager.set_pre_order(**pre_order_params)
-    
+
     # Get order parameters
     order_params = data_cfg['order_instance']
     order_args = order_params.pop('args_params')
-    order_kwargs = order_params.pop('kwargs_params')
+    # order_kwargs = order_params.pop('kwargs_params')
     # Set order configuration
-    strat_manager.set_order(*order_args, **order_params, **order_kwargs)
+    strat_manager.set_order(*order_args, **order_params)
 
     # The bot start to run
     try:
         for t in strat_manager(*strat_args, **strat_kwargs):
-            print('{}th iteration'.format(t))
+            print(r'${}^{th}$ iteration'.format(t))
             # TODO : request data_base
-            # data = data_manager.get_data(*data_requests_args, **data_requests_kwargs)
-            # TODO : clean data
+            data_manager = data_manager.get_data(
+                *data_requests_args, **data_requests_kwargs
+            )
+            # TODO : clean data or data is already cleaned ?
             # TODO : compute, print and save some statistics
         else:
             print('All is good')
@@ -78,10 +81,11 @@ def run_bot(strat_id, path='../strategies/'):
         print(txt)
         with open('errors/{}.log'.format(sys.argv[0]), 'a') as f:
             f.write(txt)
-    
+
     finally:
         # TODO : ending with save some statistics and others
         print('\nBot stoped\nSee you soon')
+
 
 if __name__ == '__main__':
     run_bot(sys.argv[1])
