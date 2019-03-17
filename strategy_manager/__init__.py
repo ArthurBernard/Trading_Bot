9a55241a5b1761d730cb9a19f6156aa421573acc
ArthurBernard <arthur.bernard.92@gmail.com>
2019-03-17 18:35:10 +0100

    remove useless import

--- strategy_manager/__init__.py
+++ strategy_manager/__init__.py
@@ -3,23 +3,14 @@
 
 from . import tools
 from .tools import *
-from . import data_loader
-from .data_loader import *
 from . import orders_manager
 from .orders_manager import *
 from . import data_requests
 from .data_requests import *
 from . import manager
 from .manager import *
-from . import request_kraken
-from .request_kraken import *
-from . import request_bitfinex
-from .request_bitfinex import *
 
 __all__ = tools.__all__
-__all__ += data_loader.__all__
 __all__ += data_requests.__all__
 __all__ += orders_manager.__all__
 __all__ += manager.__all__
-__all__ += request_kraken.__all__
-__all__ += request_bitfinex.__all__
