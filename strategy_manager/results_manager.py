77037de62830e28e8486b14b648fdc36a9b60c4c
ArthurBernard <arthur.bernard.92@gmail.com>
2019-03-25 07:46:49 +0100

    add set_order_result function

--- strategy_manager/results_manager.py
+++ strategy_manager/results_manager.py
@@ -20,6 +20,35 @@ TODO:
 """
 
 
+def set_order_result(order_result):
+    """ Clean the output of set order method.
+
+    Parameters
+    ----------
+    order_result : dict
+        Output of set order.
+
+    Returns
+    -------
+    order_result : dict
+        Cleaned result of an output order.
+
+    """
+    list_ord = order_result.pop('descr')['order'].split(' ')
+
+    order_result = {
+        'type': list_ord[0],
+        'volume': list_ord[1],
+        'pair': list_ord[2],
+        'ordertype': list_ord[4],
+        'price': list_ord[5],
+        'leverage': 1 if len(list_ord) == 6 else list_ord[7][0],
+        **order_result
+    }
+
+    return order_result
+
+
 def print_results(out):
     now = time.strftime('%y-%m-%d %H:%M:%S', time.gmtime(time.time()))
     txt = ''
