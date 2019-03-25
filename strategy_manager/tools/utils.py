20c3f4081afd6a972d4604fd0b9f44bbce651784
ArthurBernard <arthur.bernard.92@gmail.com>
2019-03-25 08:43:11 +0100

    add get_df and save_df util functions

--- strategy_manager/tools/utils.py
+++ strategy_manager/tools/utils.py
@@ -3,15 +3,17 @@
 
 # Import built-in packages
 import yaml
+from pickle import Pickler, Unpickler
 
 # Import external packages
+import pandas as pd
 
 
-__all__ = ['load_config_params']
+__all__ = ['load_config_params', 'save_df', 'get_df']
 
 
 def load_config_params(path):
-    """ Function to load configuration parameters for strategy manager. 
+    """ Function to load configuration parameters for strategy manager.
 
     Parameters
     ----------
@@ -32,6 +34,51 @@ def load_config_params(path):
     with open(path, 'r') as f:
         data = yaml.load(f)
     return data
+
+
+def get_df(path, name, ext=''):
+    """ Load a dataframe as binnary file.
+
+    Parameters
+    ----------
+    path, name, ext : str
+        Path to the file, name of the file and the extension of the file.
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        A dataframe, if file not find return an empty dataframe.
+
+    """
+    if path[-1] != '/' and name[0] != '/':
+        path += '/'
+    if len(ext) > 0 and ext[0] != '.':
+        ext = '.' + ext
+    try:
+        with open(path + name + ext, 'rb') as f:
+            df = Unpickler(f).load()
+            return df
+    except FileNotFoundError:
+        return pd.DataFrame()
+
+
+def save_df(df, path, name, ext=''):
+    """ Save a dataframe as a binnary file.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        A dataframe to save as binnary file.
+    path, name, ext : str
+        Path to the file, name of the file and the extension of the file.
+
+    """
+    if path[-1] != '/' and name[0] != '/':
+        path += '/'
+    if len(ext) > 0 and ext[0] != '.':
+        ext = '.' + ext
+    with open(path + name + ext, 'wb') as f:
+        Pickler(f).dump(df)
 
 
 if __name__ == '__main__':
