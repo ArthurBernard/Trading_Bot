6b1e3741e72c19caac99a486be12ac0ea9b60387
ArthurBernard <arthur.bernard.92@gmail.com>
2019-03-27 08:52:24 +0100

    fix type of volume and price

--- strategy_manager/results_manager.py
+++ strategy_manager/results_manager.py
@@ -8,7 +8,7 @@ import time
 import pandas as pd
 
 # Internal packages
-from tools.utils import get_df, save_df
+from strategy_manager.tools.utils import get_df, save_df
 
 __all__ = [
     'set_order_result', 'set_order_results', 'set_order_hist',
@@ -44,10 +44,10 @@ def set_order_result(order_result):
 
     order_result = {
         'type': list_ord[0],
-        'volume': list_ord[1],
+        'volume': float(list_ord[1]),
         'pair': list_ord[2],
         'ordertype': list_ord[4],
-        'price': list_ord[5],
+        'price': float(list_ord[5]),
         'leverage': 1 if len(list_ord) == 6 else list_ord[7][0],
         **order_result
     }
@@ -71,6 +71,8 @@ def set_order_results(order_results):
     """
     clean_order_result = []
     for result in order_results:
+        if result is None:
+            continue
         clean_order_result += [set_order_result(result)]
     else:
         return clean_order_result
@@ -121,7 +123,7 @@ def update_order_hist(order_result, name, path='.'):
 
     """
     # TODO : Save by year ? month ? day ?
-    # TODO : Don't save per strategy ? 
+    # TODO : Don't save per strategy ?
     # Get order historic dataframe
     df_hist = get_df(path, name + '_ord_hist', '.dat')
     # Set new order historic dataframe
