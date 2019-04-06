367d9cce7f8892b6930e0c398e9d948f5356d731
ArthurBernard <arthur.bernard.92@gmail.com>
2019-04-06 17:43:10 +0200

    add DataExchangeManager object

--- strategy_manager/data_requests.py
+++ strategy_manager/data_requests.py
@@ -17,9 +17,9 @@ import numpy as np
 from strategy_manager.tools.time_tools import now
 
 __all__ = [
-    'DataRequests', 'data_base_requests', 'aggregate_data', 'DataManager',
+    'DataRequests', 'data_base_requests', 'aggregate_data', 'DataBaseManager',
     'set_dataframe', 'get_ohlcv', 'get_ohlcv_kraken', 'save_data',
-    'update_data',
+    'update_data', 'DataExchangeManager',
 ]
 
 """
@@ -95,13 +95,17 @@ class DataRequests:
         # Set timestamp of the observation
         self.last_ts = int(time.time())
         url = self.url
+
         for arg in args:
             url += '/' + arg
+
         # Requests data
         ans = requests.get(url, kwargs)
+
         # Returns result
         try:
             return json.loads(ans.text)
+
         except Exception as error:
             txt = '\nUNKNOWN ERROR\n'
             txt += 'In {} script, at {}, '.format(
@@ -111,9 +115,12 @@ class DataRequests:
             txt += 'the following error occurs: {}\n'.format(
                 str(error)
             )
+
             with open('errors/{}.log'.format(sys.argv[0]), 'a') as f:
                 f.write(txt)
+
             time.sleep(1)
+
             return self.get_data(**kwargs)
 
     def __iter__(self):
@@ -123,17 +130,21 @@ class DataRequests:
         # Stop iteration
         if self.t >= self.stop_step:
             raise StopIteration
+
         # Sleep
         elif self.last_ts + self.time_step > time.time():
             time.sleep(self.last_ts + self.time_step - time.time())
+
         # Run
         self.t += 1
+
         return self.get_data(**self.kwargs)
 
     def __call__(self, time_step=2, **kwargs):
         # Set timestep and request's parameters
         self.time_step = time_step
         self.kwargs = kwargs
+
         return self
 
 
@@ -212,22 +223,28 @@ def _subdata_base_requests(asset, ohlcv, frequency, start, end, path):
     if start is None:
         path_file = _get_last_file(path + asset)
         df = _data_base_requests(path_file, slice(None), ohlcv)
+
         if frequency > 60:
             df = aggregate_data(df, frequency // 60)
+
         return df.iloc[-1:, :]
+
     else:
         row_slices = _set_row_slice(start, end, frequency)
         row_slice = row_slices.pop(0)
         date = time.strftime('%y-%m-%d', time.gmtime(row_slice[0]))
         path_file = path + asset + '/' + date + '.dat'
         df = _data_base_requests(path_file, row_slice, ohlcv)
+
         for row_slice in row_slices:
             date = time.strftime('%y-%m-%d', time.gmtime(row_slice[0]))
             path_file = path + asset + '/' + date + '.dat'
             subdf = _data_base_requests(path_file, row_slice, ohlcv)
             df.append(subdf)
+
         if frequency > 60:
             df = aggregate_data(df, frequency // 60)
+
         return df
 
 
@@ -235,9 +252,11 @@ def _data_base_requests(path, row_slice, col_slice):
     # Load data base
     with open(path, 'rb') as f:
         df = Unpickler(f).load()
+
     # Return specified data
     if isinstance(row_slice, tuple):
         row_slice = slice(*row_slice)
+
     return df.loc[row_slice, col_slice]
 
 
@@ -245,19 +264,25 @@ def _set_row_slice(start, end, frequency):
     i = 0
     row_slice = []
     STOP = (end - start) // 86400
+
     while i <= STOP:
         last = (start // 86400 + 1) * 86400
+
         if frequency <= 60:
             row_slice += [(start, min(last, end))]
+
         else:
             row_slice += [range(start, min(last, end), frequency)]
+
         start += last
         i += 1
+
     return row_slice
 
 
 def _get_last_file(path):
     files = listdir(path)
+
     return path + '/' + max(files)
 
 
@@ -284,17 +309,21 @@ def aggregate_data(df, win):
     for c in df.columns:
         if c == 'h':
             df.loc[::-1, c] = df.loc[::-1, c].rolling(win, min_periods=0).max()
+
         elif c == 'l':
             df.loc[::-1, c] = df.loc[::-1, c].rolling(win, min_periods=0).min()
+
         elif c == 'c':
             df.loc[:, c] = df.loc[:, c].shift(-win).fillna(method='ffill')
+
         elif c == 'v':
             df.loc[::-1, c] = df.loc[::-1, c].rolling(win, min_periods=0).sum()
+
     return df
 
 
-class DataManager:
-    """ Object to manage requests to data base.
+class DataBaseManager:
+    """ Object to request and manage data from a database.
 
     Attributes
     ----------
@@ -362,8 +391,10 @@ class DataManager:
             last = int(time.time() // self.frequency * self.frequency)
         else:
             last += self.frequency  # 60 is may be enought ?
+
         if start is None:
             start = last - self.frequency * (self.n_min_obs + 1)
+
         return data_base_requests(
             self.assets.copy(), self.ohlcv, self.frequency,
             start=start, end=last, path=self.path
@@ -404,11 +435,14 @@ def set_dataframe(data, rename={}, index=None, drop=None):
     """
     df = pd.DataFrame(np.array(data, dtype=np.float64))
     df.rename(columns=rename, inplace=True)
+
     if index is not None:
         df.set_index(index, inplace=True)
         df.index = df.index.astype(int)
+
     if drop is not None:
         df.drop(columns=drop, inplace=True)
+
     return df
 
 
@@ -439,6 +473,7 @@ def get_ohlcv(exchange, pair, since=None, frequency=60):
         )
     else:
         raise ValueError('Unknow exchange: ', exchange)
+
     return data
 
 
@@ -469,6 +504,7 @@ def get_ohlcv_kraken(asset, since=None, frequency=60):
         index='TS',
         drop=[5, 7]
     )
+
     # Return without the current observation
     return data.drop(index=now())
 
@@ -488,10 +524,13 @@ def save_data(data, asset, path='data_base/'):
     """
     if path[-1] != '/':
         path += '/'
+
     day = (data.index // 86400)
     files = range(day.min(), day.max() + 1)
+
     for file in files:
         name = time.strftime('%y-%m-%d', time.gmtime(file * 86400))
+
         with open(path + asset + '/' + name + '.dat', 'wb') as f:
             Pickler(f).dump(data.loc[data.index // 86400 == file])
 
@@ -510,20 +549,61 @@ def update_data(exchange, asset, path='data_base/'):
 
     """
     since = now(freq=86400)
+
     # Load data
     try:
         df = data_base_requests(asset, 'ohlcv', start=since, frequency=60)
         since = df.index[-1]
     except FileNotFoundError:
         df = pd.DataFrame()
+
     if exchange.lower() == 'kraken':
         data = get_ohlcv_kraken(asset, since=since, frequency=60)
     else:
         raise ValueError('Unknow exchange', exchange)
+
     # Update data
     df = df.append(data).drop_duplicates()
     # Save updated data
     save_data(df, asset, path=path)
+
+
+class DataExchangeManager:
+    """ Object to request and manage data from an exchange.
+
+    /! CURRENTLY WORK ONLY WITH KRAKEN EXCHANGE /!
+
+    """
+
+    def __init__(self, assets, path="https://api.kraken.com/0/public",
+                 frequency=None, n_min_obs=1, ohlcv='ohlcv'):
+        self.assets = assets
+        self.req = DataRequests(path, stop_step=1)
+        self.frequency = frequency
+        self.n_min_obs = n_min_obs
+        self.ohlcv = [i for i in ohlcv]
+
+    def get_data(self, *args, **kwargs):
+        data = self._get_data(*args, **kwargs)
+        data = self.clean_data(data)
+
+    # TODO : to finish
+    def _get_data(self, *args, **kwargs):
+        data = self.req.get_data(*args, **kwargs)['result']
+        return data
+
+    def clean_data(self, data):
+        # TODO : to finish
+        # to make copy of asset
+        # to loop for several assets
+        rename = {0: 'TS', 1: 'o', 2: 'h', 3: 'l', 4: 'c', 6: 'v'}
+        df = set_dataframe(data[self.assets], index='TS',
+                           rename=rename, drop=[5, 7])
+        df = df.loc[df.index % self.frequency == 0, self.ohlcv]
+        if df.index[-1] < now() - self.frequency:
+            raise ValueError('Too old data: ', df.index[-1])
+        else:
+            return df.loc.values[-self.n_min_obs:]
 
 
 if __name__ == '__main__':
