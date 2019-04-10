869583885a842b0cb58d5e5011b11c96daa7eec3
ArthurBernard <arthur.bernard.92@gmail.com>
2019-04-10 23:41:00 +0200

    add KrakenCLient and BitfinexClient to __all__

--- strategy_manager/__init__.py
+++ strategy_manager/__init__.py
@@ -11,9 +11,15 @@ from . import manager
 from .manager import *
 from . import results_manager
 from .results_manager import *
+from . import API_kraken
+from .API_kraken import *
+from . import API_bfx
+from .API_bfx import *
 
 __all__ = tools.__all__
 __all__ += data_requests.__all__
 __all__ += orders_manager.__all__
 __all__ += manager.__all__
 __all__ += results_manager.__all__
+__all__ += API_kraken.__all__
+__all__ += API_bfx.__all__
