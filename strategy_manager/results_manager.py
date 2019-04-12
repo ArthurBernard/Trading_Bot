d97f779bde528550868947e51e4a99730efc0420
ArthurBernard <arthur.bernard.92@gmail.com>
2019-04-12 23:38:25 +0200

    add fees and cumulative retrun to update_result_hist function

--- strategy_manager/results_manager.py
+++ strategy_manager/results_manager.py
@@ -6,6 +6,7 @@ import time
 
 # External packages
 import pandas as pd
+import numpy as np
 
 # Internal packages
 from strategy_manager.tools.utils import get_df, save_df
@@ -41,6 +42,7 @@ def set_order_result(order_result):
 
     """
     descr = order_result.pop('descr')
+
     if descr is not None:
         list_ord = descr['order'].split(' ')
         order_result.update({
@@ -51,8 +53,11 @@ def set_order_result(order_result):
             'price': float(list_ord[5]),
             'leverage': 1 if len(list_ord) == 6 else list_ord[7][0],
         })
+
         return order_result
+
     else:
+
         return order_result
 
 
@@ -71,16 +76,24 @@ def set_order_results(order_results):
 
     """
     clean_order_result = []
+
     for result in order_results:
         clean_order_result += [set_order_result(result['result'])]
+
     else:
+
         return clean_order_result
 
 
 def print_results(out):
-    now = time.strftime('%y-%m-%d %H:%M:%S', time.gmtime(time.time()))
-    txt = ''
-    txt += '\nAt {}: {}\n'.format(now, str(out))
+    now_ts = time.strftime('%y-%m-%d %H:%M:%S', time.gmtime(time.time()))
+    txt = '\nAt {}:\n\n'.format(now_ts)
+    i = 1
+
+    for o in out:
+        txt += '{}th order: {}\n\n'.format(i, o)
+        i += 1
+
     print(txt)
 
 
@@ -108,7 +121,6 @@ def set_order_hist(order_result):
         'timestamp', 'txid', 'userref', 'price', 'volume',
         'type', 'pair', 'ordertype', 'leverage'
     ])
-    print(df_hist.head())
 
     return df_hist
 
@@ -124,13 +136,18 @@ def update_order_hist(order_result, name, path='.'):
     """
     # TODO : Save by year ? month ? day ?
     # TODO : Don't save per strategy ?
+    if path[-1] != '/':
+        path += '/'
+
     # Get order historic dataframe
-    df_hist = get_df(path, name + '_ord_hist', '.dat')
+    df_hist = get_df(path, name + 'orders_hist', '.dat')
+    
     # Set new order historic dataframe
     df_hist = df_hist.append(set_order_hist(order_result), sort=False)
     df_hist = df_hist.reset_index(drop=True)
+    
     # Save order historic dataframe
-    save_df(df_hist, path, name + '_ord_hist', '.dat')
+    save_df(df_hist, path, name + 'orders_hist', '.dat')
 
 
 def set_results(order_results):
@@ -148,14 +165,19 @@ def set_results(order_results):
 
     """
     aggr_res = {}
+
     for result in order_results:
         ts = result['timestamp']
+
         if ts not in aggr_res.keys():
             aggr_res[ts] = {'volume': 0, 'position': 0, 'price': 0}
+
         aggr_res[ts]['volume'] += result['current_volume']
         aggr_res[ts]['position'] += result['current_position']
         aggr_res[ts]['price'] = result['price']
+
     else:
+
         return pd.DataFrame(aggr_res).T
 
 
@@ -173,14 +195,18 @@ def get_result_hist(name, path='.'):
         A dataframe of results strategy.
 
     """
-    df = get_df(path, name + '_res_hist', '.dat')
+    df = get_df(path, name + 'result_hist', '.dat')
+
     if df.empty:
+
         return pd.DataFrame(columns=['price', 'volume', 'position', 'return'])
+
     else:
+
         return df
 
 
-def update_result_hist(order_results, name, path='.'):
+def update_result_hist(order_results, name, path='.', fee=0.0016):
     """ Load, merge and save result historic strategy.
 
     Parameters
@@ -191,19 +217,30 @@ def update_result_hist(order_results, name, path='.'):
         Path and name of the file to load.
 
     """
+    if path[-1] != '/':
+        path += '/'
+
     # Get result historic
     hist = get_result_hist(name, path=path)
     df = set_results(order_results)
-    print(df.head())
+
     # Merge result historics
     hist = hist.append(df, sort=False)
     idx = hist.index
+
     if idx.size <= 1:
         hist.loc[idx[0], 'return'] = 0
+
     else:
         p = hist.loc[:, 'price'].values
         vol = hist.loc[:, 'volume'].values
         pos = hist.loc[:, 'position'].values
-        hist.loc[idx[1]:, 'return'] = (p[1:] - p[:-1]) * vol[:-1] * pos[:-1]
+        fees = fee * (pos[:-1] - pos[1:])
+        ret = (p[1:] - p[:-1]) * vol[:-1] * pos[:-1]
+        hist.loc[idx[1]:, 'return_raw'] = ret
+        hist.loc[idx[1]:, 'return_net'] = ret * (1 - fees)
+        hist.loc[idx[1]:, 'cum_return'] = np.cumsum(ret * (1 - fees))
+        # hist.loc[idx[1]:, '']
+
     # Save order historic dataframe
-    save_df(hist, path, name + '_res_hist', '.dat')
+    save_df(hist, path, name + 'result_hist', '.dat')
