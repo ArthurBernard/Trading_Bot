16c88266422b482e5fb7749e13f759073231feaa
ArthurBernard <arthur.bernard.92@gmail.com>
2019-04-25 23:34:49 +0200

    fix outputs get_order_params function

--- strategies/another_example/strategy.py
+++ strategies/another_example/strategy.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+# Import external packages
 import numpy as np
-from strategy_manager.data_requests import DataRequests
+
+# Import internal packages
+from tools.utils import load_config_params
 
 
 __all__ = ['get_signal']
@@ -10,10 +13,16 @@ __all__ = ['get_signal']
 
 def get_order_params(data, *args, **kwargs):
     """ Return signal, price and volume """
-    signal = get_signal(data, *args, **kwargs)
-    price = get_price(data, signal, *args, **kwargs)
+    # Get parameters
+    data_cfg = load_config_params(
+        './strategies/another_example/configuration.yaml'
+    )
+    params = data_cfg['order_instance']
 
-    return signal, price, 1.
+    # Set signal
+    signal = get_signal(data, *args, **kwargs)
+
+    return signal, params
 
 
 def get_signal(*args, **kwargs):
@@ -25,23 +34,3 @@ def example_random_strat(**kwargs):
     """ A dummy exemple that return a random signal """
     signals = [-1, 0, 1]
     return np.random.choice(signals)
-
-
-def get_price(data, signal, *args, **kwargs):
-    """ Compute price """
-    req = DataRequests("https://api.kraken.com/0/public", stop_step=1)
-    ans = req.get_data('Ticker', pair='XBTUSD')
-    marge = float(np.random.rand(1) - 0.4) / 10.
-
-    if signal > 0:
-        price = float(ans['result']['XXBTZUSD']['a'][0])
-        price /= 1 + marge
-
-    elif signal < 0:
-        price = float(ans['result']['XXBTZUSD']['b'][0])
-        price *= 1 + marge
-
-    else:
-        price = float(ans['result']['XXBTZUSD']['c'][0])
-
-    return round(price, 0)
