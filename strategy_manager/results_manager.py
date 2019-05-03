65edc28f454d68d0eed2d3a55017ea091785cfc8
ArthurBernard <arthur.bernard.92@gmail.com>
2019-05-03 18:35:23 +0200

    add ResultManager object

--- strategy_manager/results_manager.py
+++ strategy_manager/results_manager.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # coding: utf-8
+# @Author: ArthurBernard
+# @Email: arthur.bernard.92@gmail.com
+# @Date: 2019-05-02 19:07:38
+# @Last modified by: ArthurBernard
+# @Last modified time: 2019-05-03 17:40:31
 
 # Built-in packages
 import time
@@ -7,13 +12,12 @@ import time
 # External packages
 import pandas as pd
 import numpy as np
+import fynance as fy
 
 # Internal packages
 from strategy_manager.tools.utils import get_df, save_df
 
 __all__ = [
-    'set_order_result', 'set_order_results', 'set_order_hist',
-    'update_order_hist',
 ]
 
 """
@@ -99,23 +103,6 @@ def set_order_results(order_results):
         return clean_order_result
 
 
-def print_results(out):
-    now_ts = time.strftime('%y-%m-%d %H:%M:%S', time.gmtime(time.time()))
-    txt = '\nAt {}:\n\n'.format(now_ts)
-    i = 1
-
-    for o in out:
-        txt += '{}th order: {}\n\n'.format(i, o)
-        i += 1
-
-    print(txt)
-
-
-def set_statistic():
-    # TODO : set stats, profit and loss, etc
-    pass
-
-
 def set_order_hist(order_result):
     """ Set dataframe of historic order.
 
@@ -164,6 +151,124 @@ def update_order_hist(order_result, name, path='.'):
     save_df(df_hist, path, name + 'orders_hist', '.dat')
 
 
+class ResultManager:
+    """ Print some statistics of result historic strategy.
+
+    Parameters
+    ----------
+    path : str
+        Path of the file to load and save results.
+    init_vol : float, optional
+        Initial value invested to the strategy.
+    period : int, optional
+        Number of period per year, default is 252 (trading days).
+
+    """
+
+    def __init__(self, path='.', init_vol=1., period=252):
+        if path[-1] != '/':
+            path += '/'
+
+        self.path = path
+        self.init_vol = init_vol
+        self.period = period
+        self.df = get_df(path, 'result_hist', ext='.dat')
+
+    def update_result_hist(self, order_results):
+        """ Load, merge and save result historic strategy.
+
+        Parameters
+        ----------
+        order_results : list of dict
+            Cleaned result of one or several output order.
+
+        """
+        df = set_results(order_results)
+
+        if self.df.empty:
+            df.loc[:, 'value'] = self.init_vol
+
+        else:
+            df = self.df.iloc[-1:].append(df, sort=False)
+            df = df.fillna(method='ffill')
+            df.loc[:, 'value'] += set_performance(df)
+
+        self.df = self.df.iloc[:-1].append(df, sort=False)
+
+        return self
+
+    def save_result_hist(self):
+        """ Save result historic """
+        save_df(self.df, self.path, 'result_hist', ext='.dat')
+
+    def print_stats(self):
+        """ Print some statistics of result historic strategy.
+
+        """
+        last_ts = self.df.index[-1]
+
+        bool_index = self.df.index >= self.df.index[-1] - 86400
+        txt = self.set_stats_result(self.df.loc[bool_index], 'Daily Perf.')
+
+        print(txt)
+
+    def set_stats_result(self, df, head):
+        """ Compute stats `backward` seconds in past """
+        txt = self.set_text2(
+            ['-'] * 6,
+            [head, 'Return', 'Perf.', 'Sharpe', 'Calmar', 'MaxDD'],
+            ['-'] * 6,
+            ('Underlying', *self.set_statistics(df.price.values)),
+            ('Strategy', *self.set_statistics(df.value.values)),
+            ['-'] * 6,
+        )
+
+        return txt
+
+    def set_statistics(self, series):
+        perf = series[-1] - series[0]
+        pct = perf / series[0]
+        sharpe = fy.sharpe(series, period=self.period)
+        calmar = fy.calmar(series, period=self.period)
+        maxdd = fy.mdd(series)
+
+        return rounder(perf, pct, sharpe, calmar, maxdd, dec=2)
+
+    def set_text(self, *args):
+        stats = list(zip(*args))
+        n = len(args[0])
+        txt = ''
+
+        for arg in args:
+            txt += '\n| '
+
+            for i in range(n):
+                n_spa = max(len(str(a)) for a in stats[i]) - len(str(arg[i]))
+                txt += str(arg[i]) + ' ' * n_spa + ' |'
+
+        return txt
+
+    def set_text2(self, *args):
+        n, k = max(len(arg) for arg in args), len(args)
+        k_list = ['| ' if len(arg[0]) > 1 else '+' for arg in args]
+
+        for i in range(n):
+            n_spa = max(len(str(arg[i])) for arg in args)
+            j = 0
+
+            for arg in args:
+                if len(arg[0]) > 1:
+                    space = ' ' * (n_spa - len(str(arg[i])))
+                    k_list[j] += str(arg[i]) + space + ' | '
+
+                else:
+                    k_list[j] += arg[0] * (n_spa + 2) + '+'
+
+                j += 1
+
+        return '\n'.join(k_list)
+
+
 def set_results(order_results):
     """ Aggregate and set results.
 
@@ -196,111 +301,18 @@ def set_results(order_results):
         return pd.DataFrame(aggr_res).T
 
 
-def get_result_hist(name, path='.'):
-    """ Load result historic strategy.
-
-    Parameters
-    ----------
-    path, name : str
-        Path and name of the file to load.
-
-    Returns
-    -------
-    df : pandas.DataFrame
-        A dataframe of results strategy.
-
-    """
-    df = get_df(path, name + 'result_hist', '.dat')
-
-    if df.empty:
-
-        return pd.DataFrame(columns=['price', 'volume', 'position', 'fee'])
-
-    else:
-
-        return df
-
-
-def update_result_hist(order_results, name, path='.', fee=0.0016):
-    """ Load, merge and save result historic strategy.
-
-    Parameters
-    ----------
-    order_results : list of dict
-        Cleaned result of one or several output order.
-    path, name : str
-        Path and name of the file to load.
-
-    """
-    if path[-1] != '/':
-        path += '/'
-
-    # Get result historic
-    hist = get_result_hist(name, path=path)
-    df = set_results(order_results)
-
-    # Merge result historics
-    hist = hist.append(df, sort=False)
-    idx = hist.index
-
-    # if idx.size <= 1:
-    #    hist.loc[idx[0], 'return'] = 0
-
-    if False:
-        p = hist.loc[:, 'price'].values
-        vol = hist.loc[:, 'volume'].values
-        pos = hist.loc[:, 'position'].values
-        fees = fee * (pos[:-1] - pos[1:])
-        ret = (p[1:] - p[:-1]) * vol[:-1] * pos[:-1]
-        hist.loc[idx[1]:, 'return_raw'] = ret
-        hist.loc[idx[1]:, 'return_net'] = ret * (1 - fees)
-        hist.loc[idx[1]:, 'cum_return'] = np.cumsum(ret * (1 - fees))
-        # hist.loc[idx[1]:, '']
-
-    # Save order historic dataframe
-    save_df(hist, path, name + 'result_hist', '.dat')
-
-
-def print_stats(name, path='.', volume=1.):
-    """ Print some statistics of result historic strategy.
-
-    Parameters
-    ----------
-    path, name : str
-        Path and name of the file to load.
-
-    """
-    df = get_result_hist(name, path=path)
-    last_ts = df.index[-1]
-    ui_perf, strat_perf = set_stats(df.loc[df.index >= last_ts - 86400])
-    txt = set_text(
-        'Daily Returns',
-        Strategy=(strat_perf, 0.),
-        Underlying=(ui_perf, 0.)
-    )
-    # txt = '\nUnderlying perf : {}\n'.format(ui_perf)
-    # txt += 'Strategy perf : {}\n'.format(strat_perf)
-    print(txt)
-
-
-def set_stats(df):
+def set_performance(df):
+    """ Compute performance of a strategy """
     p = df.loc[:, 'price'].values
+    ret = np.zeros([p.size])
     vol = df.loc[:, 'volume'].values
     pos = df.loc[:, 'position'].values
     fee = df.loc[:, 'fee'].values
     fees = fee[:-1] * (pos[:-1] - pos[1:])
-    ret = (p[1:] - p[:-1]) * vol[:-1] * pos[:-1]
+    ret[1:] = (p[1:] - p[:-1]) * vol[:-1] * pos[:-1]
 
-    return p[0] - p[-1], np.sum(ret * (1 - fees))
-    # hist.loc[idx[1]:, 'return_raw'] = ret
-    # hist.loc[idx[1]:, 'return_net'] = ret * (1 - fees)
-    # hist.loc[idx[1]:, 'cum_return'] = np.cumsum(ret * (1 - fees))
+    return np.cumsum(ret * (1 - fees))
 
 
-def set_text(head, **kwargs):
-    bound = '+' + '-' * 22 + ('+' + '-' * 9) * 2 + '+\n'
-    txt = '| {:^20} | {:7} | {:7} |\n'.format(head, 'Return', 'Pct Ret')
-    for key, arg in kwargs.items():
-        txt += '| {:20} | {:7.2f} | {:7.2%} |\n'.format(key, *arg)
-
-    return bound + txt + bound
+def rounder(*args, dec=0):
+    return (round(arg, dec) for arg in args)
