a6b64b573a15e82d56de019738ea3d3a97a2baa3
ArthurBernard <arthur.bernard.92@gmail.com>
2019-05-14 22:37:00 +0200

    todo reinvest profit

--- strategy_manager/main.py
+++ strategy_manager/main.py
@@ -4,7 +4,7 @@
 # @Email: arthur.bernard.92@gmail.com
 # @Date: 2019-05-03 17:36:22
 # @Last modified by: ArthurBernard
-# @Last modified time: 2019-05-13 19:32:00
+# @Last modified time: 2019-05-13 20:08:35
 
 """ Run a bot following a configuration file. """
 
@@ -72,9 +72,9 @@ def run_bot(id_strat, path='strategies/'):
             # Print some statistics
             RM.print_stats()
             # TODO : reinvest profit option
-            # if reinvest_parameter:
-            #     new_value = RM.get_current_value()
-            #     data_cfg['order_instance']['volume'] = new_value
+            # if data_cfg['result_instance']['reinvest_profit']:
+            #     new_volume = RM.get_current_volume()
+            #     data_cfg['order_instance']['volume'] = round(new_volume, 6)
 
             # Get current pos
             current_pos = float(OM.current_pos)
