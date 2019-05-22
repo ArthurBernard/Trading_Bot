2c8492ffe6a7bd868cc6679b1d3ec738eaf69bd6
ArthurBernard <arthur.bernard.92@gmail.com>
2019-05-22 08:30:49 +0200

    add module to display bot results

---
+++ strategy_manager/get_results.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# @Author: ArthurBernard
+# @Email: arthur.bernard.92@gmail.com
+# @Date: 2019-05-19 13:05:40
+# @Last modified by: ArthurBernard
+# @Last modified time: 2019-05-22 08:26:21
+
+""" Read and print dataframe results of a strategy bot. """
+
+# Built-in packages
+import logging
+import sys
+
+# External packages
+
+# Local packages
+from results_manager import ResultManager
+from tools.utils import load_config_params
+
+
+def print_results(id_strat, n_last_results=None, path='strategies/'):
+    """ Display statistics of a bot results for a specified configuration.
+
+    Parameters
+    ----------
+    strat_id : str
+        A strat id is the name of the corresonding configuration file.
+    n_last_results : int, optional
+        Number of lines of dataframe results to display. Default is `None`.
+    path : str, optional
+        Path to load the configuration file.
+
+    """
+    logger = logging.getLogger('strat_man.' + __name__)
+
+    if path[-1] != '/':
+        path += '/'
+
+    # Load data configuration
+    logger.info('Load parameters of {}'.format(id_strat))
+    data_cfg = load_config_params(path + id_strat + '/configuration.yaml')
+
+    # Set result manager configuration
+    RM = ResultManager(**data_cfg['result_instance'])
+
+    # Print some statistics
+    RM.print_stats()
+
+    if n_last_results is not None:
+        logger.info('Historic result:\n\n' + str(RM.df.tail(n_last_results)))
+
+
+if __name__ == '__main__':
+
+    import logging.config
+    import yaml
+
+    with open('./strategy_manager/logging.ini', 'rb') as f:
+        config = yaml.safe_load(f.read())
+
+    logging.config.dictConfig(config)
+
+    args = sys.argv[1:].copy()
+    id_strat = args.pop(0)
+
+    if not args:
+        n_last_results = None
+    elif len(args) == 1:
+        n_last_results = int(args[0])
+    else:
+        raise ValueError('Unkown parameters : {}'.format(args))
+
+    print_results(id_strat, n_last_results=n_last_results)
