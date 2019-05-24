89980ecc2ff3704f7ec5994d232c943664c9a794
ArthurBernard <arthur.bernard.92@gmail.com>
2019-05-24 23:11:45 +0200

    improve strategy bot

--- execution_scripts/bot.sh
+++ execution_scripts/bot.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# @Author: ArthurBernard
+# @Email: arthur.bernard.92@gmail.com
+# @Date: 2019-04-18 23:52:54
+# @Last modified by: ArthurBernard
+# @Last modified time: 2019-05-24 23:10:11
 #
-# Script to run automatically a Python scripts and verify each 
-# second that program didn't shutdown. 
+# Script to run automatically a Python scripts while one day and verify
+# each second that program didn't shutdown. 
 # Only one parameters, `name_strategy` to execute the strategy.
 #
 # Example:
@@ -9,8 +14,12 @@
 # ./bot.sh example
 #
 
+# Set path
+path=./strategies/$1
+
 # Run `execution_strat.py` python script
-python3 strategy_manager/main.py $1 >> bot_$1.log 2>&1 &
+# python3 strategy_manager/main.py $1 >> bot_$1.log 2>&1 &
+python3 strategy_manager/main.py $1 > $path/execution.log 2>&1 &
 
 # Check the PID
 script_pid=`ps -f | grep main.py\ $1 | grep -v grep | awk '{print $2}'`
@@ -20,7 +29,7 @@ echo "$1 has started, this pid is $script_pid"
 ts=`date +%s`
 
 # Define stop 
-let "stop = ts - ts % 3600 + 3570"
+let "stop = ts - ts % 86400 + 86370"
 
 # Define shutdown counter
 let "i = 0"
@@ -31,11 +40,16 @@ while [ $ts -lt $stop ]; do
     # Check if script is always running
     if ! ps -p $script_pid > /dev/null; then
         # Program shutdown
-        echo 'Program has stopped'
+        echo '$1 strategy has stopped'
         let "i = i + 1"
 
+        # Save logs
+        date +%H:%M:%S >> $path/error_`date +%y-%m-%d`.log
+        cat $path/execution.log >> $path/error_`date +%y-%m%-%d`.log
+
         # Run `execution_strat.py` python script
-        python3 strategy_manager/main.py $1 >> bot_$1.log 2>&1 &
+        python3 strategy_manager/main.py $1 > $path/execution.log 2>&1 &
+        # python3 strategy_manager/main.py $1 >> bot_$1.log 2>&1 &
 
         # Check the PID
         script_pid=`ps -f | grep main.py\ $1 | grep -v grep | awk '{print $2}'`
