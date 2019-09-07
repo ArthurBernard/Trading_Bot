f684e4eaf78908933756d30f5beafc76de002273
ArthurBernard <arthur.bernard.92@gmail.com>
2019-09-07 11:25:17 +0200

    update tools module

--- strategy_manager/tools/__init__.py
+++ strategy_manager/tools/__init__.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
-# coding: utf-8 
+# coding: utf-8
+# @Author: ArthurBernard
+# @Email: arthur.bernard.92@gmail.com
+# @Date: 2019-03-23 12:47:31
+# @Last modified by: ArthurBernard
+# @Last modified time: 2019-09-07 11:22:57
 
+""" Module with some tools. """
+
+# Built-in packages
+
+# Third party packages
+
+# Local packages
 from . import utils
 from .utils import *
-from . import time_tools 
+from . import time_tools
 from .time_tools import *
 
 __all__ = utils.__all__
