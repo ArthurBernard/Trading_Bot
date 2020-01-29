b59bc7f1c54f42135ce1773ed25302b3b51c37c4
ArthurBernard <arthur.bernard.92@gmail.com>
2020-01-29 21:33:25 +0100

    add bot and order manager client objects

--- trading_bot_manager/_client.py
+++ trading_bot_manager/_client.py
@@ -4,9 +4,12 @@
 # @Email: arthur.bernard.92@gmail.com
 # @Date: 2020-01-28 16:47:55
 # @Last modified by: ArthurBernard
-# @Last modified time: 2020-01-28 19:47:14
+# @Last modified time: 2020-01-29 15:08:35
+
+""" Clients to connect to TradingBotServer. """
 
 # Built-in packages
+import os
 
 # Third party packages
 
@@ -15,18 +18,35 @@ from _server import TradingBotServer as TBS
 
 
 class _Client:
+    """ Base class for a client. """
+
     def __init__(self, address=('', 50000), authkey=b'tradingbot'):
         """ Initialize a client object and connect to the TradingBotServer. """
+        print('Module {}: process ID is {} and parent PID is {}'.format(
+            __name__, os.getpid(), os.getppid()
+        ))
         TBS.register('get_queue_orders')
-        TBS.register('get_proxy_fees')
-        TBS.register('_get_pipe_om_tbm')
+        TBS.register('get_state')
         self.m = TBS(address=address, authkey=authkey)
         self.m.connect()
         self.q_ord = self.m.get_queue_orders()
+        self.p_state = self.m.get_state()
+
+    def is_stop(self):
+        return self.p_state._getvalue()['stop']
+
+
+class _BotClient(_Client):
+    """ Base class for a trading bot. """
+
+    def __init__(self, address=('', 50000), authkey=b'tradingbot'):
+        """ Initialize a client object and connect to TradingBotServer. """
+        TBS.register('get_proxy_fees')
+        _Client.__init__(self, address=address, authkey=authkey)
         self.p_fees = self.m.get_proxy_fees()
 
-    def _get_fees(self, pair, order_type):
-        """ Get current fees of order.
+    def get_fee(self, pair, order_type):
+        """ Get current the fee for a pair and an order type.
 
         Parameters
         ----------
@@ -38,17 +58,29 @@ class _Client:
         Returns
         -------
         float
-            Fees of specified pair and order type.
+            Fee of specified pair and order type.
 
         """
         if order_type == 'market':
 
-            return float(self.fees['fees'][pair]['fee'])
+            return float(self.p_fees._getvalue()['fees'][pair]['fee'])
 
         elif order_type == 'limit':
 
-            return float(self.fees['fees_maker'][pair]['fee'])
+            return float(self.p_fees._getvalue()['fees_maker'][pair]['fee'])
 
         else:
 
             raise ValueError('Unknown order type: {}'.format(order_type))
+
+
+class _OrderManagerClient(_Client):
+    """ Base class for an order manager. """
+
+    def __init__(self, address=('', 50000), authkey=b'tradingbot'):
+        """ Initialize a client object and connect to TradingBotServer. """
+        TBS.register('get_reader_tbm')
+        TBS.register('get_writer_tbm')
+        _Client.__init__(self, address=address, authkey=authkey)
+        self.r_tbm = self.m.get_reader_tbm()
+        self.w_tbm = self.m.get_writer_tbm()
