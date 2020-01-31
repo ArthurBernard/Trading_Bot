a35d92fba689a3b0723dbfa285cc02f1e4240662
ArthurBernard <arthur.bernard.92@gmail.com>
2020-01-31 16:08:00 +0100

    add context manager

--- trading_bot_manager/strategy_manager.py
+++ trading_bot_manager/strategy_manager.py
@@ -4,7 +4,7 @@
 # @Email: arthur.bernard.92@gmail.com
 # @Date: 2019-05-12 22:57:20
 # @Last modified by: ArthurBernard
-# @Last modified time: 2020-01-30 18:46:20
+# @Last modified time: 2020-01-31 15:54:27
 
 """ Client to manage a financial strategy. """
 
@@ -13,14 +13,15 @@ import time
 import importlib
 import logging
 from os import getpid, getppid
+import sys
 
 # External packages
 
 # Local packages
 # from strategy_manager import DataBaseManager, DataExchangeManager
-from data_requests import get_close, DataBaseManager, DataExchangeManager
-from tools.time_tools import now
-from tools.utils import load_config_params  # , dump_config_params, get_df
+from data_requests import get_close  # , DataBaseManager, DataExchangeManager
+from tools.time_tools import now, str_time
+from tools.utils import load_config_params, dump_config_params  # , get_df
 from _client import _BotClient
 
 __all__ = ['StrategyManager']
@@ -38,6 +39,10 @@ class StrategyManager(_BotClient):
         Set parameters to pass into function' strategy.
     set_iso_vol(series, target_vol=0.2, leverage=1., period=252, half_life=11)
         Computes and returns iso-volatility coefficient.
+    get_order_params(data, *args, **kwargs)
+        Function to get signal strategy and additional parameters to set order.
+        `data` must be a pd.DataFrame, `*args` and `**kwargs` can be any
+        parameters.
 
     Attributes
     ----------
@@ -47,13 +52,11 @@ class StrategyManager(_BotClient):
         Name of the underlying or list of data needed.
     name_strat : str
         Name of the strategy to run.
-    _get_order_params : function
-        Function to get signal strategy and additional parameters to set order.
 
     """
 
     # TODO : Load strategy config
-    def __init__(self, name_strat, path, STOP=None, address=('', 50000),
+    def __init__(self, name_strat, STOP=None, address=('', 50000),
                  authkey=b'tradingbot'):
         """ Initialize strategy manager.
 
@@ -61,9 +64,6 @@ class StrategyManager(_BotClient):
         ----------
         name_strat : str
             Name of the strategy to run.
-        path : str
-            Path to load configuration, functions and any scripts needed to run
-            the strategy.
         STOP : int, optional
             Number of iteration before stoping, if None it will load it in
             configuration file.
@@ -83,71 +83,12 @@ class StrategyManager(_BotClient):
         strat = importlib.import_module(
             'strategies.' + name_strat + '.strategy'
         )
-        self._get_order_params = strat.get_order_params
+        self.get_order_params = strat.get_order_params
         self.logger.info('Strategy function loaded')
 
-        # Load configuration
         self.STOP = STOP
         self.name_strat = name_strat
-        self.set_configuration(path + name_strat + '/configuration.yaml')
-
         self.logger.info('{} initialized'.format(name_strat))
-
-    def set_configuration(self, path):
-        """ Set configuration.
-
-        Parameters
-        ----------
-        path : str
-            Path to load the YAML configuration file.
-
-        """
-        self.logger.info('Load configuration of {}'.format(self.name_strat))
-        data_cfg = load_config_params(path)
-
-        # Set general parameters and strategy state
-        self.underlying = data_cfg['strat_manager_instance']['underlying']
-        self.iso_vol = data_cfg['strat_manager_instance']['iso_volatility']
-        self.frequency = data_cfg['strat_manager_instance']['frequency']
-        self.id_strat = data_cfg['strat_manager_instance']['id_strat']
-        self.current_pos = data_cfg['strat_manager_instance']['current_pos']
-        self.current_vol = data_cfg['strat_manager_instance']['current_vol']
-        if self.STOP is None:
-            self.STOP = data_cfg['strat_manager_instance']['STOP']
-
-        # Set data manager configuration
-        self.set_data_manager(**data_cfg['get_data_instance'].copy())
-
-        # Set parameters for strategy function
-        self.f_args = data_cfg['strategy_instance']['args_params']
-        self.f_kwrds = data_cfg['strategy_instance']['kwargs_params']
-
-        # Set parameters for orders
-        self.ord_kwrds = data_cfg['order_instance']
-
-        # TODO : Set ResultManager
-        self.logger.info('Strategy is configured')
-
-    def start_loop(self, condition=True):
-        """ Run a loop until condition is false. """
-        # test = True
-        for output in self:
-            # while condition:
-            if output:
-                self.logger.info('Executed order : {}'.format(output))
-
-            txt = time.strftime('%y-%m-%d %H:%M:%S')
-            txt += ' | Next signal in {:.1f}'.format(self.next - time.time())
-            # if self.p_fees._getvalue():
-            #    txt = 'Fees received | ' + txt
-            #    if test:
-            #        test = False
-            #        self.logger.debug(self.p_fees._getvalue())
-
-            print(txt, end='\r')
-            time.sleep(0.01)
-
-        self.logger.info('StrategyManager stopped.')
 
     def __call__(self, *args, **kwargs):
         """ Set parameters of strategy.
@@ -194,160 +135,202 @@ class StrategyManager(_BotClient):
         server_stop = self.is_stop()
         strat_stop = self.t >= self.STOP
         if server_stop or strat_stop:
-            self.logger.info('Server stop : {}'.format(server_stop))
-            self.logger.info('Strategy stop : {}'.format(strat_stop))
+            self.logger.debug('Server stop : {}'.format(server_stop))
+            self.logger.debug('Strategy stop : {}'.format(strat_stop))
 
             raise StopIteration
 
         self.TS = int(time.time())
-
-        # Sleep until ready
-        # if self.next > self.TS:
-        #    time.sleep(self.next - self.TS)
         if self.next <= self.TS:
-
             self.next += self.frequency
             self.t += 1
             self.logger.info('{}th iter. over {}'.format(self.t, self.STOP))
             self.logger.info('Stop in {:.0f}\'.'.format(self.time_stop()))
-
             # TODO : Debug/find solution to request data correctly.
             #        Need to choose between request a database, server,
             #        exchange API or other.
             data = self.DM.get_data(*self.args_data, **self.kwargs_data)
 
-            return self.get_order_params(data)
+            return self.get_order_params(data, *self.f_args, **self.f_kwrds)
 
-        return []
+        return None, None
 
-    def get_order_params(self, data):
+    def __enter__(self):
+        """ Enter. """
+        # TODO : Load precedent data
+        # self.set_config(self.path)
+
+        return self
+
+    def set_config(self, path):
+        """ Set configuration.
+
+        Parameters
+        ----------
+        path : str
+            Path to load the YAML configuration file.
+
+        """
+        self.logger.info('set_config | Strat {}'.format(self.name_strat))
+        self.cfg = load_config_params(path)
+        # Set general configuration
+        self._get_general_cfg(self.cfg['strat_manager_instance'])
+        # Set data manager configuration
+        self.set_data_manager(**self.cfg['get_data_instance'].copy())
+        # Set stategy parameters
+        self._strat_cfg(self.cfg['strategy_instance'])
+        # Set parameters for orders
+        self.ord_kwrds = self.cfg['order_instance']
+        # TODO : Set ResultManager
+        self.logger.info('set_config | Strategy is configured')
+
+    def _get_general_cfg(self, strat_cfg):
+        # Get general parameters and strategy state
+        self.underlying = strat_cfg['underlying']
+        self.iso_vol = strat_cfg['iso_volatility']
+        self.frequency = strat_cfg['frequency']
+        self.id_strat = strat_cfg['id_strat']
+        self.current_pos = strat_cfg['current_pos']
+        self.current_vol = strat_cfg['current_vol']
+        self.logger.info('_get_general_cfg | pos: {}'.format(self.current_pos))
+        self.logger.info('_get_general_cfg | vol: {}'.format(self.current_vol))
+        if self.STOP is None:
+            self.STOP = strat_cfg['STOP']
+
+    def _strat_cfg(self, strat_cfg):
+        # Set parameters for strategy function
+        self.f_args = strat_cfg['args_params']
+        self.f_kwrds = strat_cfg['kwargs_params']
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        """ Exit. """
+        self.logger.info('exit | Save configuration')
+        # TODO : Save data
+        self.cfg['strat_manager_instance']['current_pos'] = self.current_pos
+        self.cfg['strat_manager_instance']['current_vol'] = self.current_vol
+        self.logger.info('exit | pos: {}'.format(self.current_pos))
+        self.logger.info('exit | vol: {}'.format(self.current_vol))
+        dump_config_params(
+            self.cfg,
+            'strategies/' + self.name_strat + '/configuration.yaml'
+        )
+        if exc_type is not None:
+            self.logger.error('exit | {}: {}\n{}'.format(
+                exc_type, exc_value, exc_tb
+            ))
+
+        self.logger.info('exit | end')
+
+    def set_order(self, s, **kwargs):
         """ Compute signal and additional parameters to set order.
 
         Parameters
         ----------
-        data : pandas.DataFrame
-            Data to compute signal' strategy.
-
-        Returns
-        -------
-        signal : float
+        s : {-1, 0, 1}
             Signal of strategy.
-        params : tuple
+        **kwargs : keyword arguments
             Parameters for order, e.g. volume, order type, etc.
 
         """
         out = []
-        signal, kw = self._get_order_params(data, *self.f_args, **self.f_kwrds)
-
-        # Don't move
-        if self.current_pos == signal:
-            self.logger.info('Position not moved')
-
-            return []
-
         # Up move
-        elif self.current_pos <= 0. and signal >= 0:
-            kw['type'] = 'buy'
-            out += self.cut_short(signal, **kw.copy(), **self.ord_kwrds.copy())
-            out += self.set_long(signal, **kw.copy(), **self.ord_kwrds.copy())
+        if self.current_pos <= 0. and s >= 0 and self.current_pos != s:
+            kwargs['type'] = 'buy'
+            if self.current_pos < 0:
+                out += self._cut_short(s, **kwargs.copy())
+
+            if s > 0:
+                out += self._set_long(s, **kwargs.copy())
 
         # Down move
-        elif self.current_pos >= 0. and signal <= 0:
-            kw['type'] = 'sell'
-            out += self.cut_long(signal, **kw.copy(), **self.ord_kwrds.copy())
-            out += self.set_short(signal, **kw.copy(), **self.ord_kwrds.copy())
+        elif self.current_pos >= 0. and s <= 0 and self.current_pos != s:
+            kwargs['type'] = 'sell'
+            if self.current_pos > 0:
+                out += self._cut_long(s, **kwargs.copy())
+
+            if s < 0:
+                out += self._set_short(s, **kwargs.copy())
 
         return out
 
-    def cut_short(self, signal, **kwargs):
+    def _cut_short(self, signal, **kwargs):
         """ Cut short position. """
-        if self.current_pos < 0:
-            # Set leverage to cut short
-            leverage = kwargs.pop('leverage')
-            kwargs['leverage'] = 2 if leverage is None else leverage + 1
+        # Set leverage to cut short
+        leverage = kwargs.pop('leverage')
+        kwargs['leverage'] = 2 if leverage is None else leverage + 1
 
-            # Set volume to cut short
-            kwargs['volume'] = self.current_vol
+        # Set volume to cut short
+        kwargs['volume'] = self.current_vol
 
-            # Query order
-            result = self.send_order(**kwargs)
+        # Query order
+        result = self.send_order(**kwargs)
 
-            # Set current volume and position
-            self.current_vol = 0.
-            self.current_pos = 0
-            result['current_volume'] = 0.
-            result['current_position'] = 0
-            self.logger.info('Short position cutted')
-
-        else:
-            result = self._set_output(kwargs)
+        # Set current volume and position
+        self.current_vol = 0.
+        self.current_pos = 0.
+        result['current_volume'] = 0.
+        result['current_position'] = 0.
+        self.logger.info('_cut_short | pos: {}'.format(self.current_pos))
 
         return [result]
 
-    def set_long(self, signal, **kwargs):
+    def _set_long(self, signal, **kwargs):
         """ Set long order. """
-        if signal > 0:
-            result = self.send_order(**kwargs)
+        result = self.send_order(**kwargs)
 
-            # Set current volume
-            self.current_vol = kwargs['volume']
-            self.current_pos = signal
-            result['current_volume'] = self.current_vol
-            result['current_position'] = signal
-            self.logger.info('Long position placed')
-
-        else:
-            result = self._set_output(kwargs)
+        # Set current volume
+        self.current_vol = kwargs['volume']
+        self.current_pos = float(signal)
+        result['current_volume'] = self.current_vol
+        result['current_position'] = signal
+        self.logger.info('_set_long | pos: {}'.format(self.current_pos))
 
         return [result]
 
-    def cut_long(self, signal, **kwargs):
+    def _cut_long(self, signal, **kwargs):
         """ Cut long position. """
-        if self.current_pos > 0:
-            # Set volume to cut long
-            kwargs['volume'] = self.current_vol
+        # Set volume to cut long
+        kwargs['volume'] = self.current_vol
 
-            # Query order
-            result = self.send_order(**kwargs)
+        # Query order
+        result = self.send_order(**kwargs)
 
-            # Set current volume
-            self.current_vol = 0.
-            self.current_pos = 0
-            result['current_volume'] = 0.
-            result['current_position'] = 0
-            self.logger.info('Long position cutted')
-
-        else:
-            result = self._set_output(kwargs)
+        # Set current volume
+        self.current_vol = 0.
+        self.current_pos = 0.
+        result['current_volume'] = 0.
+        result['current_position'] = 0.
+        self.logger.info('_cut_long | pos: {}'.format(self.current_pos))
 
         return [result]
 
-    def set_short(self, signal, **kwargs):
+    def _set_short(self, signal, **kwargs):
         """ Set short order. """
-        if signal < 0:
-            # Set leverage to short
-            leverage = kwargs.pop('leverage')
-            kwargs['leverage'] = 2 if leverage is None else leverage + 1
-            result = self.send_order(**kwargs)
+        # Set leverage to short
+        leverage = kwargs.pop('leverage')
+        kwargs['leverage'] = 2 if leverage is None else leverage + 1
+        result = self.send_order(**kwargs)
 
-            # Set current volume
-            self.current_vol = kwargs['volume']
-            self.current_pos = signal
-            result['current_volume'] = self.current_vol
-            result['current_position'] = signal
-            self.logger.info('Short position placed')
-
-        else:
-            result = self._set_output(kwargs)
+        # Set current volume
+        self.current_vol = kwargs['volume']
+        self.current_pos = float(signal)
+        result['current_volume'] = self.current_vol
+        result['current_position'] = signal
+        self.logger.info('_set_short | pos: {}'.format(self.current_pos))
 
         return [result]
 
     def send_order(self, **kwargs):
-        """ Send the ID of strategy and order parameters to OrdersManager. """
+        """ Send the ID of strategy and order parameters to OrdersManager.
+
+        Parameters
+        ----------
+        **kwargs : keyword arguments
+            Parameters for order, e.g. volume, order type, etc.
+
+        """
         self.q_ord.put((self.id_strat, kwargs))
-        self.logger.debug(
-            'Set order | Strat {} | Params {}'.format(self.id_strat, kwargs)
-        )
+        self.logger.info('send_order | Params {}'.format(kwargs))
 
         return self._set_output(kwargs)
 
@@ -395,16 +378,8 @@ class StrategyManager(_BotClient):
             self.kwargs_data = {}
 
         request_from = kwargs.pop('source_data').lower()
-
-        if request_from == 'exchange':
-            self.DM = DataExchangeManager(**kwargs)
-        elif request_from == 'database':
-            self.DM = DataBaseManager(**kwargs)
-        else:
-            raise ValueError('request_data must be exchange or database.'
-                             'Not {}'.format(request_from))
-
-        self.logger.info('DataManager initialized')
+        self.DM = self._handler[request_from](**kwargs)
+        self.logger.info('set_data_manager | {} init'.format(request_from))
 
         return self
 
@@ -421,6 +396,24 @@ class StrategyManager(_BotClient):
 
         return max(time_stop - time.time() % self.frequency, 0)
 
+    def start_loop(self):
+        """ Run a loop until condition is false. """
+        for s, kw in self:
+            if s is not None:
+                self.logger.info('Signal: {} | Parameters: {}'.format(s, kw))
+                output = self.set_order(s, **kw, **self.ord_kwrds)
+                if output:
+                    self.logger.info('Executed order : {}'.format(output))
+
+            txt = '{} | Next signal in {:.1f}'.format(
+                time.strftime('%y-%m-%d %H:%M:%S'),
+                str_time(self.next - self.TS),
+            )
+            print(txt, end='\r')
+            time.sleep(0.01)
+
+        self.logger.info('StrategyManager stopped.')
+
 
 if __name__ == '__main__':
 
@@ -432,5 +425,28 @@ if __name__ == '__main__':
 
     logging.config.dictConfig(config)
 
-    sm = StrategyManager('another_example_2', './strategies/')
-    sm.start_loop()
+    if len(sys.argv) < 2:
+        print('/!\\ RUN ANOTHER_EXAMPLE_2 /!\\')
+        name = 'another_example_2'
+
+    else:
+        name = sys.argv[1]
+
+    with StrategyManager(name) as sm:
+        sm.set_config('./strategies/' + name + '/configuration.yaml')
+        for s, kw in sm:
+            if s is not None:
+                sm.logger.info(' | Signal: {} | Parameters: {}'.format(s, kw))
+                output = sm.set_order(s, **kw, **sm.ord_kwrds)
+
+                if output:
+                    sm.logger.info(' | Executed order : {}'.format(output))
+
+            txt = time.strftime('%y-%m-%d %H:%M:%S')
+            txt += ' | Next signal in {:.1f}'.format(sm.next - time.time())
+            print(txt, end='\r')
+            time.sleep(0.01)
+
+        sm.logger.info('StrategyManager stopped.')
+
+    print('bye')
