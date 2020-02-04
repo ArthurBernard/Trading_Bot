104f9e16a33a2c893d227952fcce43c43e94dd9d
ArthurBernard <arthur.bernard.92@gmail.com>
2020-02-04 19:16:02 +0100

    add exception missing order

---
+++ trading_bot_manager/_exceptions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# @Author: ArthurBernard
+# @Email: arthur.bernard.92@gmail.com
+# @Date: 2020-02-04 16:04:55
+# @Last modified by: ArthurBernard
+# @Last modified time: 2020-02-04 16:39:20
+
+""" Define some various richly-typed exceptions. """
+
+# Built-in packages
+
+# Third party packages
+
+# Local packages
+
+
+class MissingOrderError(Exception):
+    """ Order is missing. """
+
+    def __init__(self, id_order, msg_prefix=None, params=None):
+        """ Initialize the missing order exception. """
+        msg = 'the order {} is missing'.format(id_order)
+
+        if params is not None:
+            msg += ', parameters was {}'.format(params)
+
+        if msg_prefix is not None:
+            msg = '{}: {}'.format(msg_prefix, msg)
+
+        super(Exception, self).__init__(msg)
